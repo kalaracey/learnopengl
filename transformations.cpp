c37f71b27ef8de7dba56b9da7981296ee8f960bf
Kyle Laracey <kalaracey@gmail.com>
2022-10-08 15:17:04 -0400

    Transformations

---
+++ transformations.cpp
@@ -0,0 +1,143 @@
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "common.h"
+#include "shader.h"
+#include "third_party/stb_image.h"
+
+// clang-format off
+float vertices[] = {
+    // positions           // texture coords
+     0.5f,  0.5f, 0.0f,    1.0f, 1.0f,   // top right
+     0.5f, -0.5f, 0.0f,    1.0f, 0.0f,   // bottom right
+    -0.5f, -0.5f, 0.0f,    0.0f, 0.0f,   // bottom left
+    -0.5f,  0.5f, 0.0f,    0.0f, 1.0f    // top left
+};
+
+unsigned int indices[] = {
+    // note that we start from 0!
+    0, 1, 3, // first triangle
+    1, 2, 3  // second triangle
+};
+// clang-format on
+
+void setupVertexArrayObject(unsigned int &VAO, unsigned int &VBO, unsigned int &EBO) {
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
+  // 1. Bind Vertex Array Object.
+  glBindVertexArray(VAO);
+
+  // 2. Copy our vertices array into a buffer for OpenGL to use.
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  // Copy our index array into an element buffer for OpenGL to use.
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  // 3. Then set our vertex attribute pointers.
+  // Position attribute:
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), nullptr);
+  glEnableVertexAttribArray(0);
+  // // Color attribute:
+  // glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 *
+  // sizeof(float))); glEnableVertexAttribArray(1); Texture attribute:
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+
+  // Unbind the VBO. Note that this is allowed, the call to glVertexAttribPointer registered VBO
+  // as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind.
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  // Unbind the VAO. You can unbind the VAO afterwards so other VAO calls won't accidentally
+  // modify this VAO, but this rarely happens. Modifying other VAOs requires a call to
+  // glBindVertexArray anyway, so we generally don't unbind VAOs (nor VBOs) when it's not
+  // directly necessary.
+  glBindVertexArray(0);
+
+  // Unbind the EBO. This must come after unbinding the VAO since a bound VAO stores binds and
+  // unbinds of GL_ELEMENT_ARRAY_BUFFER.
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+int main() {
+  auto app = GlfwApplication::create();
+
+  Shader shader("/Users/kal/Code/learnopengl/vertex_shader.glsl",
+                "/Users/kal/Code/learnopengl/fragment_shader.glsl");
+
+  unsigned int texture1;
+  glGenTextures(1, &texture1);
+  glBindTexture(GL_TEXTURE_2D, texture1);
+  // set the texture wrapping/filtering options (on the currently bound texture object)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  // load and generate the texture
+  int width, height, nrChannels;
+  unsigned char *data =
+      stbi_load("/Users/kal/Code/learnopengl/container.jpeg", &width, &height, &nrChannels, 0);
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    std::cout << "Failed to load container texture" << std::endl;
+  }
+  stbi_image_free(data);
+
+  unsigned int texture2;
+  glGenTextures(1, &texture2);
+  glBindTexture(GL_TEXTURE_2D, texture2);
+  // set the texture wrapping/filtering options (on the currently bound texture object)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  stbi_set_flip_vertically_on_load(true);
+  data = stbi_load("/Users/kal/Code/learnopengl/awesomeface.png", &width, &height, &nrChannels, 0);
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    std::cout << "Failed to load awesomeface texture" << std::endl;
+  }
+  stbi_image_free(data);
+
+  unsigned int VAO, VBO, EBO;
+  setupVertexArrayObject(VAO, VBO, EBO);
+
+  app->run([&]() {
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+
+    // Activate the shader before setting the uniform.
+    shader.use();
+    glUniform1i(glGetUniformLocation(shader.ID, "texture1"), 0);
+    shader.setInt("texture2", 1);
+
+    glm::mat4 trans = glm::mat4(1.0f);
+    trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+    trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+    glUniformMatrix4fv(glGetUniformLocation(shader.ID, "transform"), 1, GL_FALSE,
+                       glm::value_ptr(trans));
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
+    //////////////////////////////////////////////////
+    glBindVertexArray(0);
+  });
+
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+}
